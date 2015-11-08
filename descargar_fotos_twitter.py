984db2efd50ef83ee84dc211eaa7b47a552d58b2
4m1g0 <dev.4m1g0@gmail.com>
2015-11-08 21:56:34 +0100

    Updated to twitter version of 2015-9-8

--- descargar_fotos_twitter.py
+++ descargar_fotos_twitter.py
@@ -12,25 +12,28 @@ i = 0
 media_links = open('media_links', 'w')
 for search in searches:
     print "BUSCANDO RESULTADOS PARA: " + search
-    uri = 'https://twitter.com/i/search/timeline?f=realtime&q=' + search + '&src=typd&include_available_features=1&include_entities=1'
+    uri = 'https://twitter.com/i/search/timeline?f=tweets&vertical=default&q=' + search + '&include_available_features=1&include_entities=1&max_position=TWEET-6'
     for z in range(int(pages)):
         bundle = urlopen(uri).read()
         jtxt = json.loads(bundle)
         html = jtxt['items_html']
-        scroll_cursor = jtxt['scroll_cursor']
-        #print scroll_cursor
+        minTweet = jtxt['min_position']
+        print minTweet
         last_uri = uri
-        uri = 'https://twitter.com/i/search/timeline?f=realtime&q=' + search + '&src=typd&include_available_features=1&include_entities=1&scroll_cursor=' + scroll_cursor
+        uri = 'https://twitter.com/i/search/timeline?f=tweets&vertical=default&q=' + search + '&include_available_features=1&include_entities=1&max_position=' + minTweet
         if uri == last_uri:
             print "No hay mas resultados"
             media_links.close()
             sys.exit(0)
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(html, "lxml")
+        #print soup.prettify()
         elements = [x for x in soup.html.body.contents if x.name == 'li']
         for element in elements:
             name = element.div['data-screen-name']
             content = element.find('div', class_='content')
-            media = content.find('div', class_='js-media-container')
+            media = content.find('div', class_='js-old-media-container')
+            if not media:
+                media = content.find('div', class_='js-media-container')
             if not media:
                 print "Usuario sin media: " + name
             else: # hay media
@@ -38,38 +41,41 @@ for search in searches:
                 
                 if multi_photos: # multifoto
                     print "Usuario: " + name + " multifoto"
-                    for image in multi_photos.find_all('div'):
-                        if image['data-resolved-url-large'] + ':large' not in urls:
+                    for image in multi_photos.find_all('img'):
+                        if image['src'] + ':large' not in urls:
                             file = open(str(i) +".jpg", "w")
-                            file.write(urlopen(image['data-resolved-url-large'] + ':large').read())
-                            urls.append(image['data-resolved-url-large'] + ':large')
+                            file.write(urlopen(image['src'] + ':large').read())
+                            urls.append(image['src'] + ':large')
                             file.close()
                             print "Descargando imagen " + str(i) + ".jpg"
                             i+=1
                         else:
-                            print "Imagen " + image['data-resolved-url-large'] + ':large' + " repetida"
+                            print "Imagen " + image['src'] + ':large' + " repetida"
                 else: # otro media
-                    a = media.find('a', class_='media')
+                    images = media.findAll('div', class_='js-old-photo')
                     print "Usuario: " + name
-                    if a: # image simple
-                        image = a['data-url']
-                        if image not in urls:
-                            file = open(str(i) +".jpg", "w")
-                            file.write(urlopen(image).read())
-                            urls.append(image)
-                            file.close()
-                            print "Descargando imagen " + str(i) + ".jpg"
-                            i+=1
-                        else:
-                            print "Imagen " + image + " repetida"
+                    if len(images) > 0: # image simple
+                        for a in images:
+                            image = a['data-image-url']
+                            if image + ':large' not in urls:
+                                file = open(str(i) +".jpg", "w")
+                                file.write(urlopen(image + ":large").read())
+                                urls.append(image)
+                                file.close()
+                                print "Descargando imagen " + str(i) + ".jpg"
+                                i+=1
+                            else:
+                                print "Imagen " + image + " repetida"
                         
                     else: # video o foto externa
-                        url = media.div['data-card-url']
-                        if url not in urls:
-                            urls.append(url)
-                            print "Media URL: " + url
-                            media_links.write(url + '\n')
-                        else:
-                            print "Media URL: " + url + " repetida"
+                        card = media.find('div', class_='js-macaw-cards-iframe-container')
+                        if card:   
+                            url = 'https://twitter.com' + card['data-src']
+                            if url not in urls:
+                                urls.append(url)
+                                print "Media URL: " + url
+                                media_links.write(url + '\n')
+                            else:
+                                print "Media URL: " + url + " repetida"
 media_links.close()
 
